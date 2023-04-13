7c7aaf35580e43104e48bb02652aa7e219315a11
NIcat <nijatshh@code.edu.az>
2023-04-13 17:39:14 +0400

    Homework

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿#region MyRegion
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int IndexOf(int[] arr, int value)
+Console.WriteLine(IndexOf(numbers, 6));
+{
+    static int[] Main(int[] numbers, int a)
+    {
+    }
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == value)
+            return i;
+    }
+    return -1;
+}
+
+
+
+////#endregion
+
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+
+//Console.WriteLine(IndexOf(numbers, 5));
+//int IndexOf(int[] numbers, int value)
+//{
+
+//static int[] Main(int[] numbers,int a)
+//{
+//  for  (int i=5;i<numbers.Length;i++)
+//    {
+//       for (int i=0;i<numbers.Length;i++)
+//        Console.WriteLine(i);
+//    } 
+
+//}
